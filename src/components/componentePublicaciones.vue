34396aa6af19e4c1c01b3f9c048778531fd1007d
jose8991 <joseluisgm1998@gmail.com>
2022-09-23 18:26:12 -0500

    funcionando con setup

--- src/components/componentePublicaciones.vue
+++ src/components/componentePublicaciones.vue
@@ -1,37 +1,55 @@
 <template>
   <input type="text" v-model="texto" />
-  <button @click="postPost" class="btn btn-primary mt-4" type="submit">
+  <button @click="sendModel" class="btn btn-primary mt-4" type="submit">
     consultar
   </button>
-
-  <h1>{{ resultado }}</h1>
+  <div v-if="loading">
+    <h1>cargando</h1>
+  </div>
+  <div v-else>
+  <h1>{{ result }}</h1>
+</div>
 </template>
 
-<script>
+<script setup>
 import axios from "axios";
-export default {
-  data() {
-    return {
-      texto: "clase de programacion en la noche con cafe",
-      resultado: null
-    }
-  },
-  methods: {
-    // Pushes posts to the server when called.
-    postPost() {
-      let post = {
-      tipo: 'publicacion',
-      contenido: this.texto,
-    };
-      axios.post(`http://localhost:8000/`,post)
-      .then(response => {
-        this.resultado=response.data.respuesta
-      })
-      .catch(e => {
-        this.errors.push(e)
-      })
-    }
-  }
+import { ref,defineProps } from "vue";
+
+// const  = ref("");
+let result = ref("");
+const loading = ref(false);
+
+
+defineProps({
+  texto:String
+});
+
+let post = {
+  tipo: "publicacion",
+  contenido: texto,
+};
+
+function postPost() {
+  console.log("entro");
+
+  return axios
+    .post(`http://localhost:8000/`, post)
+    .then((response) => {
+      console.log("response");
+      result.value  = response.data.respuesta;
+      console.log(result);
+    })
+    .catch((e) => {
+      this.errors.push(e);
+    });
+}
+
+async function sendModel() {
+  loading.value = true;
+  console.log("entro a la funcion que retorna el resultado");
+  let resultModel = await postPost();
+  console.log(resultModel);
+  loading.value = false;
 }
 </script>
 
