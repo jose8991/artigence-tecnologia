254c6dbca46a912817bad7f05371545df26658b7
jose8991 <joseluisgm1998@gmail.com>
2022-09-23 16:32:41 -0500

    funcionando el post

--- src/components/componentePublicaciones.vue
+++ src/components/componentePublicaciones.vue
@@ -1,60 +1,42 @@
 <template>
-  <div class="content">
-    <div class="mb-4">
-      <textarea class="form-control py-3" id="consulta" name="consulta" type="text" placeholder="ingresa el contenido"
-        rows="4" v-model="texto"></textarea>
-    </div>
-    <div class="text-center"><button @click="consultar" class="btn btn-primary mt-4" type="submit">consultar</button></div>
-    <h1>prueba</h1>
-    <h1>{{resultado}}</h1>
-  </div>
+  <input type="text" v-model="texto" />
+  <button @click="postPost" class="btn btn-primary mt-4" type="submit">
+    consultar
+  </button>
 
+  <h1>{{ resultado }}</h1>
 </template>
-  
+
 <script>
 import axios from "axios";
 export default {
   data() {
     return {
       texto: "clase de programacion en la noche con cafe",
-      post: {
-        tipo:"publicacion",
-        contenido:this.texto
-      },
       resultado: null
     }
   },
   methods: {
-    consultar() {
-      this.log("entro a consultar");
-      axios.post("http://localhost:8000/", this.post).then((result) => {
-      this.resultado=result.data.respuesta
-      console.log(result.data.respuesta);
-    })
-      .catch((err) => {
-        console.log("err" + err);
-      });
-    }
-  },
-  created() {
-    let post = {
+    // Pushes posts to the server when called.
+    postPost() {
+      let post = {
       tipo: 'publicacion',
       contenido: this.texto,
     };
-    axios.post("http://localhost:8000/", post).then((result) => {
-      this.resultado=result.data.respuesta
-      console.log(result.data.respuesta);
-    })
-      .catch((err) => {
-        console.log("err" + err);
-      });
+      axios.post(`http://localhost:8000/`,post)
+      .then(response => {
+        this.resultado=response.data.respuesta
+      })
+      .catch(e => {
+        this.errors.push(e)
+      })
+    }
   }
-};
+}
 </script>
-  
+
 <style scoped>
 .content {
-
   height: 1200px;
   background-color: #f5f5f5;
 }
