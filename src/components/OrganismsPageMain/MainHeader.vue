45d60cac5e05ab236fa46447dd17253379ec71b7
AnyiHernandez <anyihdz525@gmail.com>
2022-10-04 16:09:42 -0500

    prueba service

--- src/components/OrganismsPageMain/MainHeader.vue
+++ src/components/OrganismsPageMain/MainHeader.vue
@@ -27,9 +27,9 @@ import Typed from "typed.js";
 
 onMounted(() => {
   const typed = new Typed(".typed", {
-    strings: ["cargando"],
-    typeSpeed: 200,
-    backSpeed: 200,
+    strings: ["analizar", "comprender", "generar"],
+    typeSpeed: 100,
+    backSpeed: 60,
     loop: true,
   });
   console.log(typed);
