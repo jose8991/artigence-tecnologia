74bb681e01e369ec4290243699a976abca901420
jose8991 <joseluisgm1998@gmail.com>
2022-10-10 19:31:42 -0500

    prueba de lista ok

--- src/components/intepolacionLineal.vue
+++ src/components/intepolacionLineal.vue
@@ -1,23 +1,24 @@
 <template>
-    <div class="main">
-          <div class="container__vector">
-            <lottie-player         class="
-          lerpw--width--i-1000px--800px--f-400px--400px--ends 
-        " src="https://lottie.host/c724746c-60b6-4611-95ac-37f509c8ea34/6bBWke5qy8.json" background="transparent" speed="1" loop autoplay></lottie-player>
-          </div>
-        </div>
-
+  <div class="main">
+    <div class="container__vector">
+      <lottie-player
+        class="lerpw--width--i-1000px--800px--f-400px--400px--ends"
+        src="https://lottie.host/c724746c-60b6-4611-95ac-37f509c8ea34/6bBWke5qy8.json"
+        background="transparent"
+        speed="1"
+        loop
+        autoplay
+      ></lottie-player>
+    </div>
+  </div>
 </template>
 
-<script setup>
+<script setup></script>
 
-</script>
-
-<style  scoped>
-.main{
-    margin: 0 auto;
-    background-color: var(--blue-footer);
-    height: 100vh;
+<style scoped>
+.main {
+  margin: 0 auto;
+  background-color: var(--blue-footer);
+  height: 100vh;
 }
-
 </style>
