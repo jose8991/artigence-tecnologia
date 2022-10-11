2c71e404625659fa7d1e1bcfebaeda72067d3da4
jose8991 <joseluisgm1998@gmail.com>
2022-10-11 11:53:10 -0500

    arreglo de responsive

--- src/components/intepolacionLineal.vue
+++ src/components/intepolacionLineal.vue
@@ -2,7 +2,10 @@
   <div class="main">
     <div class="container__vector">
       <lottie-player
-        class="lerpw--width--i-1000px--800px--f-400px--400px--ends"
+        class="
+          lerpw--width--i-1000px--800px--f-400px--400px--ends 
+          lerpw--transform--i-1000px--rotateL0degR--f-400px--rotateL360degR--ends
+        "
         src="https://lottie.host/c724746c-60b6-4611-95ac-37f509c8ea34/6bBWke5qy8.json"
         background="transparent"
         speed="1"
