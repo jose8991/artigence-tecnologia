524c528dba9ad27257b0e27e69e1ce3b4001feb2
jose8991 <joseluisgm1998@gmail.com>
2022-10-18 21:14:04 -0500

    ensayo

---
+++ src/components/PruebasEnsayos.vue
@@ -0,0 +1,25 @@
+<template>
+    <MainNavar />
+    <main>
+    <p>
+    La inteligencia artificial es una rama de la computer science que se preocupa por la creación de máquinas que piensan y actúan como los seres humanos. Esta tecnología se está utilizando cada vez más en nuestro mundo y tiene muchos beneficios. En primer lugar, la inteligencia artificial puede ayudar a mejorar la productividad en el trabajo. Por ejemplo, si una empresa tiene una gran cantidad de datos que necesitan ser analizados, pueden usar un software de inteligencia artificial para hacer el trabajo por ellos. Esto significa que los empleados pueden pasar más tiempo en otras tareas más importantes o en su vida personal. En segundo lugar, la inteligencia artificial también puede ayudar a mejorar la calidad de vida de las personas. Por ejemplo, si una persona tiene una enfermedad crónica, la inteligencia artificial puede ayudar a controlar los síntomas y mejorar la calidad de vida. También puede ayudar a las personas a tomar mejores decisiones sobre su salud. En tercer lugar, la inteligencia artificial también puede ayudar a mejorar la educación. Por ejemplo, si un niño tiene dificultades para aprender, la inteligencia artificial puede ayudar a mejorar el rendimiento escolar. También puede ayudar a los maestros a enseñar de manera más efectiva. En cuarto lugar, la inteligencia artificial también puede ayudar a mejorar la seguridad. Por ejemplo, si hay un accidente de coche, la inteligencia artificial puede ayudar a los investigadores a comprender qué pasó y cómo se puede evitar que vuelva a suceder. También puede ayudar a las personas a tomar mejores decisiones sobre la seguridad. En quinto lugar, la inteligencia artificial también puede ayudar a mejorar el medio ambiente. Por ejemplo, si hay un desastre natural, la inteligencia artificial puede ayudar a las personas a comprender qué pasó y cómo se puede evitar que vuelva a suceder. También puede ayudar a las personas a tomar mejores decisiones sobre el medio ambiente. En resumen, la inteligencia artificial tiene muchos beneficios. Puede ayudar a mejorar la productividad en el trabajo, la calidad de vida de las personas, la educación, la seguridad y el medio ambiente.</p>
+</main>
+</template>
+
+<script setup>
+import MainNavar from "@/components/MainNavar.vue";
+</script>
+
+<style  scoped>
+p{
+    font-family: 'Quicksand', sans-serif;
+    font-size: 1.5em;
+    color: white;
+    text-align: justify;
+    padding: 1em;
+}
+main{
+    background: var( --blue-intro);
+    min-height: 100vh;
+}
+</style>
